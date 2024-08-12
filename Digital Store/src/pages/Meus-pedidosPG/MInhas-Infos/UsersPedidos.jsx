e2da72197ab7ed7be208c45c530ee7a017f35cba
Davidamascen07 <davidddf.frota@gmail.com>
2024-08-12 20:47:40 -0300

    delete

--- Digital Store/src/pages/Meus-pedidosPG/MInhas-Infos/UsersPedidos.jsx
+++ Digital Store/src/pages/Meus-pedidosPG/MInhas-Infos/UsersPedidos.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react"
 
 export default function UsersPedidos() {
-    const [nome, setNome] = useState("Antony Kleber Lima")
-    const [CPF, setCPF] = useState("093.124.764.87")
-    const [email, setEmail] = useState("Antonykleber@devcoll.com.br")
-    const [cell, setCell] = useState("(85) 4002-8922")
-    const [adress, setAdress] = useState("Rua João Pessoa, 333")
-    const [Bairro, setBairro] = useState("Centro")
+    const [nome, setNome] = useState("Ricardo Silva")
+    const [CPF, setCPF] = useState("123.456.789-00")
+    const [email, setEmail] = useState("ricardo.silva@gmail.com")
+    const [cell, setCell] = useState("(85) 98765-4321")
+    const [adress, setAdress] = useState("Rua das Flores, 123")
+    const [Bairro, setBairro] = useState("Jardim")
     const [Cidade, setCity] = useState("Fortaleza, Ceará")
-    const [CEP, setCEP] = useState("433-8800")        
+    const [CEP, setCEP] = useState("600-0000")        
     
     const user = { nome, email, CPF, cell, adress, Bairro, Cidade, CEP }
 
